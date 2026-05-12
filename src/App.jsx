273f976d2ed1439f287a457df82e8bb04bdfa4d6
mahib-bit <sadmanmahib@gmail.com>
2026-05-12 17:22:19 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Countries from './components/Countries/Countries'
+
+function App() {
+
+
+  return (
+    <>
+      <section id="center">
+          <Countries />
+          <h1>World on the go</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
